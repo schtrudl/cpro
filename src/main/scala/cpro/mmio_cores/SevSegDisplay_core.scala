da999603e3de54a6258b0d312eec95f7fcff62e1
schtrudl <125376055+schtrudl@users.noreply.github.com>
2025-02-02 09:24:28 +0100

    functions in 7seg display
    Signed-off-by: schtrudl <125376055+schtrudl@users.noreply.github.com>

--- src/main/scala/cpro/mmio_cores/SevSegDisplay_core.scala
+++ src/main/scala/cpro/mmio_cores/SevSegDisplay_core.scala
@@ -72,7 +72,7 @@ class SevSegDisplay extends Module {
   val (_, anode_prescaler) =
     Counter(0 until 40000, io.enable_7seg, reset.asBool)
 
-  //    anode assert/select
+  // anode assert/select
   val (count, _) = Counter(0 until 8, anode_prescaler, reset.asBool)
   when(io.enable_7seg) {
     io.anode_select := ~(1.U << count)
@@ -80,55 +80,58 @@ class SevSegDisplay extends Module {
     io.anode_select := ~0.U
   }
 
-  //    value_to_digit
-
   val digit = Wire(UInt(4.W))
-  digit := "b0000".U
-  switch(~io.anode_select) {
-    is("h01".U) {
-      digit := io.display_data(3, 0)
-    }
-    is("h02".U) {
-      digit := io.display_data(7, 4)
-    }
-    is("h04".U) {
-      digit := io.display_data(11, 8)
-    }
-    is("h08".U) {
-      digit := io.display_data(15, 12)
-    }
-    is("h10".U) {
-      digit := io.display_data(19, 16)
-    }
-    is("h20".U) {
-      digit := io.display_data(23, 20)
-    }
-    is("h40".U) {
-      digit := io.display_data(27, 24)
-    }
-    is("h80".U) {
-      digit := io.display_data(31, 28)
+  def extract_digit(select: UInt) = {
+    digit := "b0000".U
+    switch(select) {
+      is("h01".U) {
+        digit := io.display_data(3, 0)
+      }
+      is("h02".U) {
+        digit := io.display_data(7, 4)
+      }
+      is("h04".U) {
+        digit := io.display_data(11, 8)
+      }
+      is("h08".U) {
+        digit := io.display_data(15, 12)
+      }
+      is("h10".U) {
+        digit := io.display_data(19, 16)
+      }
+      is("h20".U) {
+        digit := io.display_data(23, 20)
+      }
+      is("h40".U) {
+        digit := io.display_data(27, 24)
+      }
+      is("h80".U) {
+        digit := io.display_data(31, 28)
+      }
     }
   }
+  extract_digit(~io.anode_select)
 
-  //    digit_to_segments
-  io.segs := DontCare // this should not be needed but apparently chisel does not do completeness check
-  switch(digit) {
-    is("b0000".U) { io.segs := "b1000000".U } // 0
-    is("b0001".U) { io.segs := "b1111001".U } // 1
-    is("b0010".U) { io.segs := "b0100100".U } // 2
-    is("b0011".U) { io.segs := "b0110000".U } // 3
-    is("b0100".U) { io.segs := "b0011001".U } // 4
-    is("b0101".U) { io.segs := "b0010010".U } // 5
-    is("b0110".U) { io.segs := "b0000010".U } // 6
-    is("b0111".U) { io.segs := "b1111000".U } // 7
-    is("b1000".U) { io.segs := "b0000000".U } // 8
-    is("b1001".U) { io.segs := "b0010000".U } // 9
-    is("b1010".U) { io.segs := "b0001000".U } // A
-    is("b1011".U) { io.segs := "b0000011".U } // b
-    is("b1100".U) { io.segs := "b1000110".U } // C
-    is("b1101".U) { io.segs := "b0100001".U } // d
-    is("b1110".U) { io.segs := "b0000110".U } // E
-    is("b1111".U) { io.segs := "b0001110".U } // F
+  def digit_to_segments(digit: UInt) = {
+    io.segs := DontCare
+    switch(digit) {
+      is("b0000".U) { io.segs := "b1000000".U } // 0
+      is("b0001".U) { io.segs := "b1111001".U } // 1
+      is("b0010".U) { io.segs := "b0100100".U } // 2
+      is("b0011".U) { io.segs := "b0110000".U } // 3
+      is("b0100".U) { io.segs := "b0011001".U } // 4
+      is("b0101".U) { io.segs := "b0010010".U } // 5
+      is("b0110".U) { io.segs := "b0000010".U } // 6
+      is("b0111".U) { io.segs := "b1111000".U } // 7
+      is("b1000".U) { io.segs := "b0000000".U } // 8
+      is("b1001".U) { io.segs := "b0010000".U } // 9
+      is("b1010".U) { io.segs := "b0001000".U } // A
+      is("b1011".U) { io.segs := "b0000011".U } // b
+      is("b1100".U) { io.segs := "b1000110".U } // C
+      is("b1101".U) { io.segs := "b0100001".U } // d
+      is("b1110".U) { io.segs := "b0000110".U } // E
+      is("b1111".U) { io.segs := "b0001110".U } // F
+    }
   }
+  digit_to_segments(digit)
 }
